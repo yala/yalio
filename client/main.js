ce413a9ffc8dc9807a902234c7dae9499789b728
Adam Yala <adamyala@mit.edu>
2017-08-29 18:33:08 -0400

    adding piping for project

--- client/main.js
+++ client/main.js
@@ -1,22 +1,20 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+import './layout.tpl.jade';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Template.publications.helpers({
+	'papers' : function(){		
+		return Papers.find({})
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+	}
+})
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.paperCard.helpers({
+	'has_code' : function(){		
+		return this.code != nil
+	}
+})
+
+
+
