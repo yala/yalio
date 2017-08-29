37a8e618ec271bad5649c4de2f00cd19d6101fcf
Adam Yala <adamyala@mit.edu>
2017-08-29 18:16:26 -0400

    add data backend for papers and projecs

--- server/main.js
+++ server/main.js
@@ -1,5 +1,32 @@
-import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo'
+var fs = require('fs');
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function() {
+    
+    Papers.remove({})
+    if (Papers.find().count() === 0) {
+
+        var papers = JSON.parse(Assets.getText("papers.json"));
+
+        for (var ind in papers) {
+            Papers.insert(papers[ind]);
+        }
+    }
+    
+    Projects.remove({})
+    if (Projects.find().count() === 0) {
+
+        var projects = JSON.parse(Assets.getText("projects.json"));
+
+        for (var proj in projects) {
+            Projects.insert(projects[proj]);
+        }
+    }
+
+
+    console.log("Papers has " + Papers.find().count() + " documents.");
+    console.log("Projects has " + Projects.find().count() + " documents."); 
+
+
+
 });
